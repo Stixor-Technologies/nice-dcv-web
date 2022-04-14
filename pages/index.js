4c070005edfcb37e84b47c7706f6f1e5e6b79a12
SANAN\sanan <sananbintahir@gmail.com>
2022-04-14 15:54:12 +0500

    remove aws ui

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { useState, useEffect } from "react";
-import "@awsui/global-styles/index.css";
 import dynamic from "next/dynamic";
 
 const DCVViewerComponent = dynamic(() => import("../components/dcv"), {
